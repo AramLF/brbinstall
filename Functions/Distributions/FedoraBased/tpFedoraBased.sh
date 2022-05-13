0b8d7465d93ac9e499d954c32e097786a4aee44d
AramLF <AramLF@users.noreply.github.com>
2022-05-14 01:28:43 +0200

    update fedorabased functions

--- Functions/Distributions/FedoraBased/tpFedoraBased.sh
+++ Functions/Distributions/FedoraBased/tpFedoraBased.sh
@@ -22,6 +22,7 @@ dnf.conf tweaks
 #best=False
 #skip_if_unavailable=True
 cat /etc/dnf/dnf.conf
+sudo sh -c 'echo "# Added for speed:" >> /etc/dnf/dnf.conf'
 sudo sh -c 'echo "fastestmirror=True" >> /etc/dnf/dnf.conf'
 sudo sh -c 'echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf'
 sudo sh -c 'echo "defaultyes=True" >> /etc/dnf/dnf.conf'
@@ -35,12 +36,10 @@ echo -ne "
 enable rpm fusion free & nonfree
 ==============================================================================
 "
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 #for gnome-software...
-sudo dnf install -y rpmfusion-free-appstream-data
-sudo dnf install -y rpmfusion-nonfree-appstream-data
+sudo dnf groupupdate -y core
 }
 
 brbinstall_distrib_fedora-based_codecs () {
@@ -49,9 +48,27 @@ echo -ne "
 enable rpm media codecs
 ==============================================================================
 "
-sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-sudo dnf install -y lame\* --exclude=lame-devel
-sudo dnf group upgrade -y --with-optional Multimedia
+sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+
+sudo dnf groupupdate -y sound-and-video
+
+#old :
+#sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+#sudo dnf install -y lame\* --exclude=lame-devel
+#sudo dnf group upgrade -y --with-optional Multimedia
+}
+
+brbinstall_distrib_fedora-based_audio-pls () {
+echo -ne "
+==============================================================================
+audio could now work on vm
+==============================================================================
+"
+#use with care
+#could work idk
+systemctl --user --now disable pipewire pipewire-pulse wireplumber
+sudo dnf swap -y pipewire-pulseaudio pulseaudio --allowerasing
+sudo dnf install -y pulseaudio-utils
 }
 
 brbinstall_distrib_fedora-based_raw () {
@@ -72,9 +89,12 @@ cat /etc/os-release
 }
 
 brbinstall_distrib_fedora-based_run () {
-  ls -l
-  #later
-  #brbinstall_distrib_fedora-based_dnf_tweaks
-  #brbinstall_distrib_fedora-based_rpm_fusion
-  #brbinstall_distrib_fedora-based_codecs
+
+  brbinstall_distrib_fedora-based_dnf_tweaks
+
+  brbinstall_distrib_fedora-based_rpm_fusion
+
+  brbinstall_distrib_fedora-based_codecs
+
+  brbinstall_setup_miscsetup_flathub
 }
