fdc1a97b1713efb2ce02d0930944b49a9faff758
AramLF <AramLF@users.noreply.github.com>
2022-05-14 02:47:39 +0200

    added ende colors for gnome-terminal

--- Functions/Flavoring/matte.sh
+++ Functions/Flavoring/matte.sh
@@ -30,6 +30,8 @@ brbinstall_flavoring_matte_de(){
     brbinstall_theming_kde_catppuccin
 
     brbinstall_theming_kde-misc_konsole_all
+  elif check_command gnome-terminal; then
+    brbinstall_theming_gterminal_set_ende
   fi
 
 }
--- Functions/Theming/gterminal.sh
+++ Functions/Theming/gterminal.sh
@@ -23,3 +23,27 @@ git clone https://github.com/catppuccin/gnome-terminal $initialCacheFolder/gnome
 cd $initialCacheFolder/gnome-terminal
 echo YES |./install.sh "catppuccin"
 }
+
+brbinstall_theming_gterminal_set_ende () {
+echo -ne "
+==============================================================================
+EndeavourOS color-schemes gnome-terminal installation
+==============================================================================
+"
+echo -ne "
+If you want a personnal theme for gnome-terminal, create a new profile named
+'ende' and then press enter
+"
+read -r -p "Press Enter " response
+
+git clone https://github.com/catppuccin/gnome-terminal $initialCacheFolder/gnome-terminal-ende
+cd $initialCacheFolder/gnome-terminal-ende
+
+echo "['#08052B', '#FF7F7F', '#47B35D', '#CC3980', '#7FBAFF', '#7F3FBF', '#7F7FFF', '#CBCCBD', '#7FBAFF', '#FF7F7F', '#9999CC', '#FF7F7F', '#7F7FFF', '#7F3FBF', '#7F7FFF', '#E3E3EA']" > palette
+sed -i 's#D7DAE0#E3E3EA#g' install.sh
+sed -i '0,/1E1E2E/s//08052B/' install.sh
+sed -i 's#1E1E2E#E3E3EA#g' install.sh
+sed -i 's#F5E0DC#FF7F7F#g' install.sh
+
+echo YES |./install.sh "ende"
+}
