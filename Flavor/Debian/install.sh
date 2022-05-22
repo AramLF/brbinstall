80ad4eee7c615fae7049d3ea7c0235c95ae69fba
AramLF <AramLF@users.noreply.github.com>
2022-05-22 18:26:11 +0200

    Debian script updated

--- Flavor/Debian/install.sh
+++ Flavor/Debian/install.sh
@@ -4,34 +4,8 @@ source ./scriptSource.sh
 
 brbinstall_template_tp1_title
 
-brbinstall_distrib_debian-based_version
-
 brbinstall_template_tp1_start
 
-brbinstall_distrib_debian-based_dependencies
-
-brbinstall_theming_icon_papirus
-
-brbinstall_theming_wallpaper_all
-
-brbinstall_theming_gtk_catppuccin
-
-brbinstall_theming_gterminal_set_catppuccin
-
-brbinstall_de_gnome_del_gnome_software_startup
-
-brbinstall_de_gnome_set_gdm_tools
-
-brbinstall_de_gnome_set_icon_theme
-
-brbinstall_de_gnome_set_gtk_theme
-
-brbinstall_de_gnome_set_wallpaper
-
-brbinstall_distrib_debian-based_prompt
-
-brbinstall_distrib_debian-based_plymouth
-
-brbinstall_distrib_debian-based_cnf
+brbinstall_distrib_debian-based_run-script
 
 brbinstall_template_tp1_end
--- Functions/Distributions/DebianBased/tpDebianBased.sh
+++ Functions/Distributions/DebianBased/tpDebianBased.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-brbinstall_distrib_debian-based_version () {
-echo -ne "Debian 11
-==============================================================================
-"
-}
-
 brbinstall_distrib_debian-based_dependencies () {
 echo -ne "
 ==============================================================================
@@ -46,4 +40,66 @@ Adding alias cnf
 #alias cnf='command-not-found'
 echo "alias cnf='command-not-found'" >> ~/.bash_aliases
 sudo apt-get update
+}
+
+brbinstall_distrib_debian-based_run-script (){
+echo -ne "
+==============================================================================
+Debian running script
+==============================================================================
+"
+brbinstall_distrib_debian-based_dependencies
+
+brbinstall_de_gnome_del_gnome_software_startup
+
+brbinstall_de_gnome_set_gdm_tools
+
+brbinstall_distrib_debian-based_prompt
+
+brbinstall_distrib_debian-based_plymouth
+
+brbinstall_distrib_debian-based_cnf
+
+}
+
+brbinstall_distrib_debian-based_remove (){
+echo -ne "
+==============================================================================
+Remove apps Deb
+==============================================================================
+"
+#could change to del bloat apps for any distrib
+sudo apt purge -y gnome-2048
+sudo apt purge -y gnome-chess
+sudo apt purge -y gnome-contacts
+sudo apt purge -y gnome-mahjongg
+sudo apt purge -y gnome-maps
+sudo apt purge -y gnome-mines
+sudo apt purge -y gnome-nibbles
+sudo apt purge -y gnome-robots
+sudo apt purge -y gnome-sudoku
+sudo apt purge -y gnome-taquin
+sudo apt purge -y gnome-tetravex
+sudo apt purge -y gnome-weather
+sudo apt purge -y evolution
+sudo apt purge -y hitori
+sudo apt purge -y quadrapassel
+sudo apt purge -y four-in-a-row
+sudo apt purge -y swell-foop
+sudo apt purge -y tali
+sudo apt purge -y malcontent
+sudo apt purge -y seahorse
+sudo apt purge -y shotwell
+sudo apt purge -y gnome-todo
+sudo apt purge -y aisleriot
+sudo apt purge -y gnome-klotski
+sudo apt purge -y five-or-more
+sudo apt purge -y gnome-games*
+sudo apt purge -y lightsoff
+sudo apt purge -y iagno
+sudo apt-get remove -y --purge libreoffice*
+sudo apt-get remove -y --purge transmission*
+
+sudo apt clean -y
+sudo apt autoremove -y
 }
