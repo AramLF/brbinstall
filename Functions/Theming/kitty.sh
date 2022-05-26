b1e19ae60140647019bbe7c9d44ee24a30ba41e7
AramLF <AramLF@users.noreply.github.com>
2022-05-26 17:11:16 +0200

    added catppuccin for kitty and one rename

--- Functions/Theming/kitty.sh
+++ Functions/Theming/kitty.sh
@@ -12,8 +12,17 @@ brbinstall_setup_kitty_auto-theming-term 2C2C2C CC0000 4E9A06 C4A000 3565A4 7550
 brbinstall_theming_kitty_set-ende () {
 echo -ne "
 ==============================================================================
-kitty flue theme installation
+kitty ende theme installation
 ==============================================================================
 "
 brbinstall_setup_kitty_auto-theming-term 08052B FF7F7F 47B35D CC3980 7FBAFF 7F3FBF 7F7FFF CDCCDB 7FBAFF FF7F7F 9999CC FF7F7F 7F7FFF 7F3FBF 7F7FFF E3E3EA 08052B E3E3EA "none" "none" "none" "background"
+}
+
+brbinstall_theming_kitty_set-catppuccin () {
+echo -ne "
+==============================================================================
+kitty catppuccin theme installation
+==============================================================================
+"
+brbinstall_setup_kitty_auto-theming-term 6E6C7C F28FAD ABE9B3 FAE3B0 96CDFB DDB6F2 F5C2E7 D9E0EE 988BA2 F28FAD ABE9B3 FAE3B0 96CDFB DDB6F2 F5C2E7 D9E0EE 1E1E2E D9E0EE "none" "none" "none" "background"
 }
