5f1025c5850406d6a730466449e7d51d1fe33467
AramLF <AramLF@users.noreply.github.com>
2022-05-26 14:18:22 +0200

    quick fix kitty

--- Functions/Theming/kitty.sh
+++ Functions/Theming/kitty.sh
@@ -6,5 +6,5 @@ echo -ne "
 kitty flue theme installation
 ==============================================================================
 "
-brbinstall_setup_kitty_auto-theming-term 2C2C2C CC0000 4E9A06 C4A000 3565A4 75507B 06989A D3D7CF 555753 EF2929 8AE234 FCE94F 729FCF AD7FA8 34E2E2 EEEEEC 2C2C2C EEEEEC "none" "none" "none" "none"
+brbinstall_setup_kitty_auto-theming-term 2C2C2C CC0000 4E9A06 C4A000 3565A4 75507B 06989A D3D7CF 555753 EF2929 8AE234 FCE94F 729FCF AD7FA8 34E2E2 EEEEEC 2C2C2C EEEEEC "none" "none" "none" "background"
 }
