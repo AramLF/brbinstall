20fa3801f0a65aba9cbeb4a178ba19e47a642bfc
AramLF <AramLF@users.noreply.github.com>
2022-05-26 20:04:07 +0200

    Updated r3

--- Functions/Flavoring/r3.sh
+++ Functions/Flavoring/r3.sh
@@ -33,12 +33,14 @@ r3 creating gaps configuration
 ==============================================================================
 "
 echo -ne "
+#gaps with i3-gaps
 for_window [class=\".*\"] border pixel 2
 
 gaps inner 10
 gaps outer 10
 
 smart_gaps on
+smart_borders on
 " >> ~/.config/i3/config
 
 }
@@ -52,6 +54,7 @@ r3 feh and wallpaper
 sudo $auto_pkg_installer feh
 
 echo -ne "
+#wallpaper
 exec_always feh --bg-scale $1
 " >> ~/.config/i3/config
 
@@ -65,65 +68,23 @@ r3 picom
 "
 sudo $auto_pkg_installer picom
 #not complete
+#compton for an old picom
 echo -ne "
+#transparancy
 exec_always picom -f
 " >> ~/.config/i3/config
 #--config ~/.config/picom/picom.conf
 #--blur-background
 }
 
-brbinstall_flavoring_r3_not_yet () {
+brbinstall_flavoring_r3_client-colors () {
 echo -ne "
 ==============================================================================
-r3 for i3 not rdy yet
-==============================================================================
-"
-#none for now
-echo -ne "
-
-gaps inner 10
-gaps outer 10
-
-smart_gaps on
-
-for_window [class=\".*\"] border pixel 0
-
-set \$bg-color 	         #2f343f
-set \$inactive-bg-color   #2f343f
-set \$text-color          #f3f4f5
-set \$inactive-text-color #676E7D
-set \$urgent-bg-color     #E53935
-
-# Start i3bar to display a workspace bar (plus the system information i3status
-# finds out, if available)
-#bar {
-#	position top
-#       status_command polybar example
-#	colors {
-#		background \$bg-color
-#	    	separator #757575
-#		#                  border             background         text
-#		focused_workspace  \$bg-color          \$bg-color          \$text-color
-#		inactive_workspace \$inactive-bg-color \$inactive-bg-color \$inactive-text-color
-#		urgent_workspace   \$urgent-bg-color   \$urgent-bg-color   \$text-color
-#	}
-#}
-
-exec polybar example
-
-exec_always picom -f --config ~/.config/picom/picom.conf
-"
-}
-
-
-brbinstall_flavoring_r3_not_yet_2 () {
-echo -ne "
-==============================================================================
-r3 for i3 not rdy yet 2
+r3 client colors
 ==============================================================================
 "
-
 echo -ne "
+#setting some colors
 set \$bg-color 	         #2f343f
 set \$inactive-bg-color   #2f343f
 set \$text-color          #f3f4f5
@@ -136,40 +97,60 @@ client.focused          \$bg-color           \$bg-color          \$text-color   
 client.unfocused        \$inactive-bg-color \$inactive-bg-color \$inactive-text-color #00ff00
 client.focused_inactive \$inactive-bg-color \$inactive-bg-color \$inactive-text-color #00ff00
 client.urgent           \$urgent-bg-color    \$urgent-bg-color   \$text-color          #00ff00
+" >> ~/.config/i3/config
 
-hide_edge_borders both
-
-gaps inner 10
-gaps outer 10
-
-smart_gaps on
-
-# Start i3bar to display a workspace bar (plus the system information i3status
-# finds out, if available)
-bar {
-        status_command i3blocks -c ~/.config/i3/i3blocks.conf
-        position top
-        colors {
-		background \$bg-color
-	    	separator #757575
-		#                  border             background         text
-		focused_workspace  \$bg-color          \$bg-color          \$text-color
-		inactive_workspace \$inactive-bg-color \$inactive-bg-color \$inactive-text-color
-		urgent_workspace   \$urgent-bg-color   \$urgent-bg-color   \$text-color
-	}
 }
 
-
-bindsym \$mod+shift+x exec i3lock --color \"\$bg-color\"
-bindsym \$mod+x exec rofi -show run -lines 3 -eh 2 -width 100 -padding 800 -opacity \"85\" -bw 0 -bc \"\$bg-color\" -bg \"\$bg-color\" -fg \"\$text-color\" -hlbg \"\$bg-color\" -hlfg \"#9575cd\" -font \"System San Francisco Display 18\"
-
-
-#wallpaper
-exec_always feh --bg-scale /home/ame/Téléchargements/MK1x2W6mZzOMGcPjjTuiUPc9NskaV_eYZf959Bvm7DA.png
-
-#transparancy
-exec_always compton -f
-
+brbinstall_flavoring_r3_i3-lock () {
+echo -ne "
+==============================================================================
+r3 setting i3-lock
+==============================================================================
 "
+echo -ne "
+#i3-lock
+bindsym \$mod+shift+x exec i3lock --color \"\$bg-color\"
+" >> ~/.config/i3/config
+
+}
+
+brbinstall_flavoring_r3_polybar () {
+echo -ne "
+==============================================================================
+r3 setting polybar
+==============================================================================
+"
+#exec_always --no-startup-id $HOME/.config/polybar/launch.sh
+echo -ne "
+#polybar
+exec_always polybar
+" >> ~/.config/i3/config
+
+}
+
+brbinstall_flavoring_r3_remove-i3bar () {
+echo -ne "
+==============================================================================
+r3 remove i3bar
+==============================================================================
+"
+sed -i "s/bar {
+        status_command i3status
+}/#bar {
+#        status_command i3status
+#}/g" ~/.config/i3/config
+
+}
+
+brbinstall_flavoring_r3_rofi () {
+echo -ne "
+==============================================================================
+r3 rofi
+==============================================================================
+"
+echo -ne "
+#rofi
+bindsym \$mod+x exec rofi -show run -lines 3 -eh 2 -width 100 -padding 800 -opacity \"85\" -bw 0 -bc \"\$bg-color\" -bg \"\$bg-color\" -fg \"\$text-color\" -hlbg \"\$bg-color\" -hlfg \"#9575cd\" -font \"System San Francisco Display 18\"
+" >> ~/.config/i3/config
 
 }
