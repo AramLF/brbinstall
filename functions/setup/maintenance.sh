b59e9795ae1e43b0fd8570e6eed98db9210a88f8
AramLF <AramLF@users.noreply.github.com>
2022-09-03 19:38:47 +0200

    chroot a partition

--- functions/setup/maintenance.sh
+++ functions/setup/maintenance.sh
@@ -9,7 +9,7 @@ Echo the grub fix
 #https://askubuntu.com/questions/1317940/boot-repair-fails-to-help-the-current-session-is-in-bios-compatibility-mode
 #https://unix.stackexchange.com/questions/111889/how-do-i-update-grub-in-arch-linux
 echo -ne "
-mount /dev/sda5 /mnt
+sudo mount /dev/sda5 /mnt
 for i in /dev /dev/pts /proc /sys /run; do sudo mount -B \$i /mnt\$i; done
 sudo chroot /mnt
 #might be grub2 or another name
@@ -18,4 +18,18 @@ sudo update-grub
 #or update-grub do the grub-mkconfig
 grub-mkconfig -o /boot/grub/grub.cfg
 "
+}
+
+brbinstall_setup_maintenance_chroot-partition () {
+echo -ne "
+==============================================================================
+chroot a partition
+==============================================================================
+"
+ls /dev/sda*
+read -r -p "Select the number, [Enter] to validate : " chrootChoice 
+sudo mount /dev/sda$chrootChoice /mnt
+for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
+sudo chroot /mnt
+
 }
