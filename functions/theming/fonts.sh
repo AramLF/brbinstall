756304518073579bbd895ddd32b79c7c699e7ce9
AramLF <AramLF@users.noreply.github.com>
2022-10-08 19:11:59 +0200

    Added ubuntu fonts

--- functions/theming/fonts.sh
+++ functions/theming/fonts.sh
@@ -10,6 +10,10 @@ sudo mkdir -p /usr/share/fonts/truetype/
 sudo mkdir -p /usr/share/fonts/opentype/
 
 brbinstall_theming_fonts_fira-mozilla-all
+
+brbinstall_theming_fonts_spline-sans-all
+
+brbinstall_theming_fonts_ubuntu-all
 }
 
 brbinstall_theming_fonts_fira-mozilla-all (){
@@ -54,4 +58,24 @@ sudo cp -vr *.ttf *.TTF /usr/share/fonts/truetype/
 
 cd $initialCacheFolder/SpineSansMono/fonts/otf/
 sudo cp -vr *.otf *.OTF /usr/share/fonts/opentype/
+}
+
+brbinstall_theming_fonts_ubuntu-all (){
+echo -ne "
+==============================================================================
+Ubuntu all fonts installation
+==============================================================================
+"
+sudo mkdir -p /usr/share/fonts/truetype/
+sudo mkdir -p /usr/share/fonts/opentype/
+
+cd $initialCacheFolder/
+
+wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip
+unzip -d 0cef8205-ubuntu-font-family-0.83.zip ubuntu-font-family
+cd ubuntu-font-family/ubuntu-font-family-0.83/
+
+sudo cp -vr *.ttf *.TTF /usr/share/fonts/truetype/
+sudo cp -vr *.otf *.OTF /usr/share/fonts/opentype/
+
 }
