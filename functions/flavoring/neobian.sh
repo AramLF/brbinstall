cfc0ff1885be38bca6d5c7484d21fbdbdf3f1d20
AramLF <AramLF@users.noreply.github.com>
2023-06-18 05:03:01 +0200

    updated neobian for server

--- functions/flavoring/neobian.sh
+++ functions/flavoring/neobian.sh
@@ -55,11 +55,42 @@ sudo $auto_pkg_installer cockpit openssh-server
 
 # add the close lid part
 
+# add numlock part for tty in another function
+#https://wiki.archlinux.org/title/Activating_numlock_on_bootup
+echo -ne '#!/bin/bash
+
+for tty in /dev/tty{1..6}
+do
+    /usr/bin/setleds -D +num < "$tty";
+done
+' | sudo tee /usr/local/bin/numlock >/dev/null
+
+sudo chmod +x /usr/local/bin/numlock
+
+echo -ne '[Unit]
+Description=numlock
+
+[Service]
+ExecStart=/usr/local/bin/numlock
+StandardInput=tty
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+' | sudo tee /etc/systemd/system/numlock.service >/dev/null
+
+sudo chmod +x /etc/systemd/system/numlock.service
+
+sudo systemctl enable numlock.service
+#numlock end
+
 #server end
 
 
-#prevention
-ls
+sudo $auto_pkg_installer xfce4
+#gnome-core
+
+#sudo systemctl disable lightdm
 
 sudo $auto_pkg_installer lightdm
 
@@ -67,7 +98,7 @@ sudo systemctl enable lightdm
 
 sudo systemctl set-default graphical.target
 
-sudo $auto_pkg_installer xfce4?
+
 
 
 #!!! need to add the xfw4 light one
