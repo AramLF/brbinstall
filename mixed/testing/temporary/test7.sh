0aaf5ffaa1a1fb85f03da5d7a370e1088e7254c3
AramLF <AramLF@users.noreply.github.com>
2023-07-14 01:39:14 +0200

    trying something (test7)

---
+++ mixed/testing/temporary/test7.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd ../../../
+source ./scriptSource.sh
+
+cd mixed/testing/temporary
+#brbinstall_template_tp1_version
+
+
+
+testing-things-7 (){
+
+  brbinstall_setup_commands_script-typescript
+
+  brbinstall_template_tp1_version
+
+  brbinstall_setup_commands_script-typescript
+
+}
+
+
+echo "start"
+
+
+testing-things-7 
+
+echo "end"
